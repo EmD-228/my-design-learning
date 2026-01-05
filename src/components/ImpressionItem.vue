13a46999e7630a85a8d1db60934d36402626973b
Kokou DENYO <80518134+EmD-228@users.noreply.github.com>
2026-01-05 22:58:23 +0000

    Add home icon to ImpressionItem component for enhanced visual representation

--- src/components/ImpressionItem.vue
+++ src/components/ImpressionItem.vue
@@ -3,6 +3,7 @@
         <img src="../assets/3.png" alt="Impression">
         <div class="flex flex-col text-start gap-4 bg-white p-6">
             <h3 class="text-black">English Departement</h3>
+            <Icon icon="mdi:home" style="color: red"  :ssr="true" />
             <h3 class="text-black">Best dental surgeons</h3>
             <p class="text-black">We focus on ergonomics and meeting you where you work. It's only a keystroke away.</p>
 
