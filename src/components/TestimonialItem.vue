d4078a3bb5b76f0bd1be4ee3dfff5e16fdf1bb5b
Kokou DENYO <80518134+EmD-228@users.noreply.github.com>
2026-01-06 21:23:17 +0000

    feat: add TestimonialItem component with user rating display

---
+++ src/components/TestimonialItem.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="w-56 flex flex-nowrap flex-col items-center gap-4 bg-white p-6">
+        <img class="w-32 h-32 rounded-full" src="../assets/3.png" alt="Testimonial 1">
+        <p class="text-black">The gradual accumulation and small-scale..</p>
+        <div class="flex flex-row items-center gap-2">
+            <Icon class="" icon="mdi:star" style="color: orange" />
+            <Icon class="" icon="mdi:star" style="color: orange" />
+            <Icon class="" icon="mdi:star" style="color: orange" />
+            <Icon class="" icon="mdi:star" style="color: orange" />
+            <Icon class="" icon="mdi:star" style="color: gray" />
+        </div>
+        <span class="text-black font-bold leading-none">Regina Miles</span>
+        <span class="text-gray-500 leading-none">UX Designer</span>
+    </div>
+</template>
+<script setup lang="ts">
+import { Icon } from '@iconify/vue';
+</script>
+<style scoped>
+
+</style>
