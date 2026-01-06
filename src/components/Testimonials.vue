9df010dfcdda7d57fc236dcfbf5b36a94a400344
Kokou DENYO <80518134+EmD-228@users.noreply.github.com>
2026-01-06 21:23:14 +0000

    feat: add Testimonials component with horizontal scroll

---
+++ src/components/Testimonials.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="my-40">
+        <h2 class="text-primary ">Practice Advises</h2>
+        <h1 class="text-4xl font-bold my-6">Each and every client is important.</h1>
+        <span class="mx-16 text-center">Problems trying to resolve the conflict between the two major realms of
+            Classical physics: Newtonian mechanics </span>
+
+        <div class="flex flex-row shrink-0 gap-4 mt-6 justify-center items-center overflow-x-scroll">
+            <TestimonialItem class="col-span-1 "></TestimonialItem>
+            <TestimonialItem class="col-span-1 "></TestimonialItem>
+            <TestimonialItem class="col-span-1 "></TestimonialItem>
+            <TestimonialItem class="col-span-1 "></TestimonialItem>
+            <TestimonialItem class="col-span-1 "></TestimonialItem>
+            <TestimonialItem class="col-span-1 "></TestimonialItem>
+            <TestimonialItem class="col-span-1 "></TestimonialItem>
+        </div>
+    </div>
+</template>
+<script setup lang="ts">
+import TestimonialItem from './TestimonialItem.vue';
+</script>
