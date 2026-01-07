733f92d1da8758104656ab3f31274130b8a415d3
Kokou DENYO <80518134+EmD-228@users.noreply.github.com>
2026-01-07 21:04:08 +0000

    feat: add daisyui dependency and update Testimonials layout
    - Add daisyui as a dependency in package.json and package-lock.json
    - Update Testimonials component to use daisyui carousel for improved layout
    - Enhance styling with daisyui plugin in style.css

--- src/components/Testimonials.vue
+++ src/components/Testimonials.vue
@@ -2,19 +2,32 @@
     <div class="my-40">
         <h2 class="text-primary ">Practice Advises</h2>
         <h1 class="text-4xl font-bold my-6">Each and every client is important.</h1>
-        <span class="mx-16 text-center">Problems trying to resolve the conflict between the two major realms of
+        <span class="mx-16 text-center max-w-md">Problems trying to resolve the conflict between the two major realms of
             Classical physics: Newtonian mechanics </span>
-
-        <div class="flex flex-row shrink-0 gap-4 mt-6 justify-center items-center overflow-x-scroll">
-            <TestimonialItem class="col-span-1 "></TestimonialItem>
-            <TestimonialItem class="col-span-1 "></TestimonialItem>
-            <TestimonialItem class="col-span-1 "></TestimonialItem>
-            <TestimonialItem class="col-span-1 "></TestimonialItem>
-            <TestimonialItem class="col-span-1 "></TestimonialItem>
-            <TestimonialItem class="col-span-1 "></TestimonialItem>
-            <TestimonialItem class="col-span-1 "></TestimonialItem>
-            
+        <div class="carousel rounded-box max-w-5xl space-x-4 p-4  mt-4">
+            <div class="carousel-item">
+                <TestimonialItem></TestimonialItem>
+            </div>
+            <div class="carousel-item">
+                <TestimonialItem></TestimonialItem>
+            </div>
+            <div class="carousel-item">
+                <TestimonialItem></TestimonialItem>
+            </div>
+            <div class="carousel-item">
+                <TestimonialItem></TestimonialItem>
+            </div>
+            <div class="carousel-item">
+                <TestimonialItem></TestimonialItem>
+            </div>
+            <div class="carousel-item">
+                <TestimonialItem></TestimonialItem>
+            </div>
+            <div class="carousel-item">
+                <TestimonialItem></TestimonialItem>
+            </div>
         </div>
+
     </div>
 </template>
 <script setup lang="ts">
