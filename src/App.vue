bdf0f9c5cea71eb7de9ba56bdcea122cbb435293
Kokou DENYO <80518134+EmD-228@users.noreply.github.com>
2026-01-08 23:37:39 +0000

    feat: add Newsletters and Footer sections to App
    Import and include Newsletters and Footer components
    in the main App layout.

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,11 @@
 import DemartmentsSection from './components/DemartmentsSection.vue';
 import FAQ from './components/FAQ.vue';
 import FeaturesSection from './components/FeaturesSection.vue';
+import Footer from './components/Footer.vue';
 import HeaderSection from './components/HeaderSection.vue';
 import HeroSection from './components/HeroSection.vue';
 import ImpressionSection from './components/ImpressionSection.vue';
+import Newsletters from './components/Newsletters.vue';
 import TestimonialsSection from './components/Testimonials.vue';
 </script>
 
@@ -16,7 +18,8 @@ import TestimonialsSection from './components/Testimonials.vue';
   <ImpressionSection></ImpressionSection>
   <TestimonialsSection></TestimonialsSection>
   <FAQ></FAQ>
-
+  <Newsletters></Newsletters>
+  <Footer></Footer>
 </template>
 
 <style scoped></style>
