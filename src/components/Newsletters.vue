78f7305fc4f5ff6341ff1d5bf8016f0224ffc05b
Kokou DENYO <80518134+EmD-228@users.noreply.github.com>
2026-01-08 23:37:35 +0000

    feat: add Newsletters component with email subscription
    Add Newsletters section component with subscription form
    including email input and subscribe button.

---
+++ src/components/Newsletters.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="my-40">
+        <h2 class="text-primary ">Newletters</h2>
+        <h1 class="text-4xl font-bold my-6">Join Us</h1>
+        <span class="mx-auto">Problems trying to resolve the conflict between the two major realms of Classical physics:
+            Newtonian mechanics </span>
+        <div class="join w-full max-w-lg mx-auto mt-6">
+            <label class="input validator join-item bg-white">
+                <input class="text-black" type="email" placeholder="Your Email" required />
+            </label>
+            <div class="validator-hint hidden">Enter valid email address</div>
+            <button class="btn btn-primary join-item">subscribe</button>
+        </div>
+    </div>
+</template>
+<script setup lang="ts">
+
+</script>
