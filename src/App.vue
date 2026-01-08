47559066a6838f45e4c5bec5aab4f0618889fdc0
Kokou DENYO <80518134+EmD-228@users.noreply.github.com>
2026-01-08 23:21:15 +0000

    feat: add FAQ section to App component
    Import and include FAQ component in the main App layout.

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import DemartmentsSection from './components/DemartmentsSection.vue';
+import FAQ from './components/FAQ.vue';
 import FeaturesSection from './components/FeaturesSection.vue';
 import HeaderSection from './components/HeaderSection.vue';
 import HeroSection from './components/HeroSection.vue';
@@ -14,6 +15,7 @@ import TestimonialsSection from './components/Testimonials.vue';
   <DemartmentsSection></DemartmentsSection>
   <ImpressionSection></ImpressionSection>
   <TestimonialsSection></TestimonialsSection>
+  <FAQ></FAQ>
 
 </template>
 
