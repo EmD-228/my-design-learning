d870dfebe033be69f2ae878effce55b35ab116e8
Kokou DENYO <80518134+EmD-228@users.noreply.github.com>
2026-01-13 21:54:13 +0000

    ui: remove max-width constraint from Newsletters form
    Remove max-w-lg constraint to allow form to use full
    available width for better responsive layout.

--- src/components/Newsletters.vue
+++ src/components/Newsletters.vue
@@ -4,7 +4,7 @@
         <h1 class="text-4xl font-bold my-6">Join Us</h1>
         <span class="mx-auto">Problems trying to resolve the conflict between the two major realms of Classical physics:
             Newtonian mechanics </span>
-        <div class="join w-full max-w-lg mx-auto mt-6">
+        <div class="join w-full mx-auto mt-6">
             <label class="input validator join-item bg-white">
                 <input class="text-black" type="email" placeholder="Your Email" required />
             </label>
