e2d1acdad54d85100e3cfe8938d011e3a2a05e48
Kokou DENYO <80518134+EmD-228@users.noreply.github.com>
2026-01-15 23:34:30 +0000

    ui: improve FeaturesSection responsive layout
    - Add responsive text sizes
    - Adjust spacing and layout

--- src/components/FeaturesSection.vue
+++ src/components/FeaturesSection.vue
@@ -1,12 +1,11 @@
 <template>
-    <div class="my-40">
-        <h2 class="text-primary ">Practice Advises</h2>
-        <h1 class="text-4xl font-bold my-6">See Our Impressions</h1>
+    <div class="">
+        <p class="text-primary ">Practice Advises</p>
+        <p class="text-xl sm:text-3xl md:text-4xl lg:text-5xl font-bold my-2">See Our Impressions</p>
         <span class="mx-auto">Problems trying to resolve the conflict between the two major realms of Classical physics:
             Newtonian mechanics </span>
-
-        <div class="flex flex-row gap-4 mt-6 justify-center">
-            <FeatureItem></FeatureItem>
+        <div class="grid gap-4 mt-6 sm:grid-cols-2 justify-center">
+            <FeatureItem ></FeatureItem>
             <FeatureItem></FeatureItem>
         </div>
     </div>
